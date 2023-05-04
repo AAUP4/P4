67f890526600f1b31d43111698ef9e152fa124e1
CitronMeister <nightmarez1997@gmail.com>
2023-05-04 13:24:39 +0200

    Added game class
    Made new game class with exit print and input

---
+++ Java Examples/JavaExample/src/Game.java
@@ -0,0 +1,26 @@
+public class Game {
+    Game() {
+        System.out.println("Welcome to a card game written in CLUB!");
+    }
+
+    public void exit(Player player){
+        // Winner and scoreboard
+
+        System.exit(0);
+    }
+    public void exit(){
+        System.out.println("It is a draw!");
+        // Prints Scoreboard
+        
+
+        System.exit(0);
+    }
+    public void print(){
+
+    }
+    public void input(){
+
+    }
+
+
+}
--- Java Examples/JavaExample/src/Main.java
+++ Java Examples/JavaExample/src/Main.java
@@ -5,20 +5,9 @@ public class Main {
     public static Deck deck = new Deck();
     public static Table table = new Table();
     public static Player p = new Player("Nanna");
-
+    public static Game game = new Game();
     public static void main(String[] args) {
-        // print all 52 cards
-        System.out.println(deck.size());
-        // draws 3 cards from deck
-        p.draw(3, deck);
-        // prints deck again now 49
-        System.out.println(deck.size());
-
-        p.discardAll(discardPile);
-        System.out.println("discard pile:" + discardPile.size());
-        deck.returnDiscardPile();
-        System.out.println(deck.size());
-        System.out.println("discard pile:" + discardPile.size());
-        System.out.println("hello world");
+        
+        game.exit();
     }
 }
